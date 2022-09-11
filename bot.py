f3f68af5456c54b6641d6ad306abfe72807cda28
Dimitri Glazkov <dimitri@glazkov.com>
2022-09-10 20:29:12 -0700

    Make bot not barf on concurrent requests.

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
+import asyncio
 import logging
 import os
 import traceback
 
 import discord
 import replicate
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 logging.basicConfig()
@@ -15,11 +17,24 @@ token = os.getenv('DISCORD_TOKEN')
 embed_color = discord.Colour.from_rgb(200, 200, 100)
 bot = discord.Bot()
 
+queue = asyncio.Queue()
 
-@bot.slash_command(description="Show a picture that represents the prompt")
-async def show(ctx, prompt: str):
-    logger.info(f"asked to show: {prompt}")
-    await ctx.defer()
+
+@tasks.loop(seconds=1.0)
+async def runner():
+    task = await queue.get()
+    await task
+    queue.task_done()
+
+
+@runner.before_loop
+async def before():
+    await bot.wait_until_ready()
+
+
+async def make_image(ctx, prompt):
+    await asyncio.sleep(1.0)
+    print(f"make_image with prompt: {prompt}")
     embed = discord.Embed()
     embed.color = embed_color
     embed.set_footer(text=prompt)
@@ -33,4 +48,12 @@ async def show(ctx, prompt: str):
             title='show failed', description=f'{e}\n{traceback.print_exc()}', color=embed_color)
         await ctx.followup.send(embed=embed)
 
+
+@bot.slash_command(description="Show a picture that represents the prompt")
+async def picture(ctx, prompt: str):
+    await ctx.defer()
+    print(f"asked to picture: {prompt}")
+    queue.put_nowait(make_image(ctx, prompt))
+
+runner.start()
 bot.run(token)
