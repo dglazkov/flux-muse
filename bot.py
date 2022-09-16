e82ed82f59ede9410e0c5391e71689178ff48f09
Dimitri Glazkov <dimitri@glazkov.com>
2022-09-16 11:04:37 -0700

    Added support for GPT-3 story-telling

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 import traceback
 
 import discord
+import openai
 import replicate
 from discord.ext import tasks
 from dotenv import load_dotenv
@@ -12,7 +13,8 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 load_dotenv()
-token = os.getenv("DISCORD_API_TOKEN")
+discord_api_token = os.getenv("DISCORD_API_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_TOKEN")
 
 embed_color = discord.Colour.from_rgb(200, 200, 100)
 bot = discord.Bot()
@@ -55,5 +57,36 @@ async def picture(ctx, prompt: str):
     print(f"asked to picture: {prompt}")
     queue.put_nowait(make_image(ctx, prompt))
 
+
+async def make_story(ctx, prompt):
+    await asyncio.sleep(1.0)
+    print(f"make_story with prompt: {prompt}")
+    embed = discord.Embed()
+    embed.color = embed_color
+    embed.title = prompt
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        embed.description = response.choices[0].text
+        await ctx.followup.send(embed=embed)
+    except Exception as e:
+        embed = discord.Embed(
+            title="show failed", description=f"{e}\n{traceback.print_exc()}", color=embed_color)
+        await ctx.followup.send(embed=embed)
+
+
+@bot.slash_command(description="Tell me a story")
+async def story(ctx, prompt: str):
+    await ctx.defer()
+    print(f"asked to tell a story: {prompt}")
+    queue.put_nowait(make_story(ctx, prompt))
+
 runner.start()
-bot.run(token)
+bot.run(discord_api_token)
